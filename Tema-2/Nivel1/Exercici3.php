178a3beb689ced9ff34f2f68e748ca038110f869
franenlanube <ciscosolana@gmail.com>
2023-05-06 15:53:50 +0200

    Add despription to Exercici3

---
+++ Tema-2/Nivel1/Exercici3.php
@@ -0,0 +1,22 @@
+<html>
+
+<ol> 
+<li>Declara dues variables X i Y de tipus int, dues variables N i M de tipus double i assigna a cadascuna un valor. A continuació, mostra per pantalla per a X i Y:</li>
+<ul>
+
+<li>El valor de cada variable.</li>
+<li>La suma.</li>
+<li>La resta.</li>
+<li>El producte.</li>
+<li>El mòdul.</li>
+<li>Per N i M realitzaràs el mateix.</ul>
+
+Per a totes les variables (X, Y, N, M):
+<ul>
+<li>El doble de cada variable.</li>
+<li>La suma de totes les variables.</li>
+<li>El producte de totes les variables.</li>
+</ul>
+<li>Crea una funció Calculadora que entri dos nombres per paràmetre, i en un tercer paràmetre et permeti fer la suma, la resta, la multiplicació o la divisió dels dos nombres.</li>
+</ol>
+</html>
