5cd8ae783bbab50651f5176b0f293a126a624fe9
franenlanube <ciscosolana@gmail.com>
2023-05-06 13:12:15 +0200

    first variables declared

---
+++ Tema-2/Nivel1/Exercici1.php
@@ -0,0 +1,16 @@
+<?php 
+/* **Exercici 1** 
+Defineix una variable de cada tipus: integer, double, string i boolean. Imprimeix-les per pantalla.
+
+Després crea una constant que inclogui el teu nom i mostra-ho en format títol per pantalla. */
+
+//integer
+$intVar = 5;
+//double
+$doubleVar = 3.5;
+//String
+$StringVar = 'MyString';
+//boolean
+$booleanVar = true;
+
+?>
