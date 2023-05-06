e8e4eeac56291c8fd1664b55b7094cc50b8e6644
franenlanube <ciscosolana@gmail.com>
2023-05-06 20:11:03 +0200

    First commit with objetives of the exercise

---
+++ Tema-2/Nivel1/Exercici5.php
@@ -0,0 +1,12 @@
+<html>
+<h1>Exercici 5</h1>
+<p> Escriure una funció per verificar el grau d'un/a estudiant d'acord amb la nota.</p>
+<h3>Condicions:</h3>
+<ul>
+    <li>Si la nota és 60% o més, el grau hauria de ser Primera Divisió.</li>
+    <li>Si la nota està entre 45% i 59%, el grau hauria de ser Segona Divisió.</li>
+    <li>Si la nota està entre 33% to 44%, el grau hauria de ser Tercera Divisió.</li>
+    <li>Si la nota és menor a 33%, l'estudiant reprovarà.</li>
+</ul>
+
+</html>
