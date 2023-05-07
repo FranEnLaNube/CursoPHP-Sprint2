87340d091450980186e55242182686dce163d902
franenlanube <ciscosolana@gmail.com>
2023-05-07 21:55:41 +0200

    Name changed from T3E1 to T3N1E1.php

---
+++ Tema-3/Nivel1/T3N1E1.php
@@ -0,0 +1,8 @@
+<?php 
+//Creating $myArray
+$myArray= [2,4,6,5,9];
+//Printing $myArray with a for loop that finiches with the count() function
+for ($i=0; $i < count($myArray); $i++) { 
+    echo $myArray[$i].'<br>';
+  }
+?>
