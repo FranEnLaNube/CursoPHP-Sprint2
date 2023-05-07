56f314820b5d43b309e7c38be6b1b832d56e6115
franenlanube <ciscosolana@gmail.com>
2023-05-08 00:13:31 +0200

    Add coments

--- Tema-3/Nivel1/T3N1E2.php
+++ Tema-3/Nivel1/T3N1E2.php
@@ -1,11 +1,16 @@
 <?php 
 $X = array (10, 20, 30, 40, 50,60);
+//getting the $x size
 echo count($X);
+//deleting 4th value of $X
 unset($X[3]);
+//Creating a new Aray to sort get the index in order
 $newX = [];
+//Filling $newX with $X values
 foreach ($X as $key => $value) {
     $newX[] = $value;
 }
+//Printing $newX size
 echo '<br>';
 echo count($newX);
 ?>
