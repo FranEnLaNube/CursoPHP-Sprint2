4710f866974db8d8b73c7e4814e099d429f163da
franenlanube <ciscosolana@gmail.com>
2023-05-07 21:53:05 +0200

    Coments added

--- Tema-3/Nivel1/T3E1.php
+++ Tema-3/Nivel1/T3E1.php
@@ -1,5 +1,7 @@
 <?php 
+//Creating $myArray
 $myArray= [2,4,6,5,9];
+//Printing $myArray with a for loop that finiches with the count() function
 for ($i=0; $i < count($myArray); $i++) { 
     echo $myArray[$i].'<br>';
   }
