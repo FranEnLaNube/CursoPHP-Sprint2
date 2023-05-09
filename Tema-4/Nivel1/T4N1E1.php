6b72ca73ec87fd6853f77525782d3b9c3d90f9a8
franenlanube <ciscosolana@gmail.com>
2023-05-09 15:52:18 +0200

    Add coments and improving legibility on print()

--- Tema-4/Nivel1/T4N1E1.php
+++ Tema-4/Nivel1/T4N1E1.php
@@ -12,11 +12,14 @@ class Employee
         $this->name = $name;
         $this->wage = $wage;
     }
+    //Function print depending on the employee's wage
     function print($wage)
     {
-        return ($wage <= 6000 ? " no" : '');
+        return ($wage <= 6000 ?' no paga impuestos.' : ' paga impuestos.');
     }
 }
 //Object instance 
 $emp1 = new Employee;
 $emp1->initialize('Carlos', '7000');
+//Printing
+echo $emp1->name . $emp1->print($emp1->wage);
