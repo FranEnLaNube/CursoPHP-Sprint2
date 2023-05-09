463e924e50c0a66e77fa6cec09f5a34ce31e5e96
franenlanube <ciscosolana@gmail.com>
2023-05-09 15:45:19 +0200

    Method print created with a ternary operator

--- Tema-4/Nivel1/T4N1E1.php
+++ Tema-4/Nivel1/T4N1E1.php
@@ -1,13 +1,19 @@
-<?php 
+<?php
+
 //Creating class employee
-class employee {
+class Employee
+{
     //Atributes/properties
     public $name;
     public $wage;
     //Method 'initialize' with 'nom' and 'sou' as a parameter
-    function initialize ($name, $wage){
-        $this->name=$name;
-        $this->wage=$wage;
-    }    
+    function initialize($name, $wage)
+    {
+        $this->name = $name;
+        $this->wage = $wage;
+    }
+    function print($wage)
+    {
+        return ($wage <= 6000 ? " no" : '');
+    }
 }
-?>
