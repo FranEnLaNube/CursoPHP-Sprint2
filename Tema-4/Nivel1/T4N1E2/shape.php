16b21a026bca7f6f91d5682926bb9843cc87f0da
franenlanube <ciscosolana@gmail.com>
2023-05-09 21:26:04 +0200

    constructor created with $height and $width

--- Tema-4/Nivel1/T4N1E2/shape.php
+++ Tema-4/Nivel1/T4N1E2/shape.php
@@ -2,6 +2,18 @@
 <h1>Tema 4, Nivel 1, Exercici 2:</h1>
 <h2>Enunciado:</h2>
 <p>Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la forma area().</p>
-<h2>Solució</h2>
+<h2>Solució:</h2>
 
-<?php ?>
+<?php
+class Shape
+{
+    function __construct($height, $width)
+    {
+        $this->height = $height;
+        $this->$width = $width;
+    }
+}
+
+?>
+
+</html>
