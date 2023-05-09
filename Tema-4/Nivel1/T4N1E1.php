357f9437796d5e1f561d7698323ae0d423cb77f7
franenlanube <ciscosolana@gmail.com>
2023-05-09 15:11:58 +0200

    Method initialize created

--- Tema-4/Nivel1/T4N1E1.php
+++ Tema-4/Nivel1/T4N1E1.php
@@ -4,5 +4,10 @@ class employee {
     //Atributes/properties
     public $name;
     public $wage;
-    }
+    //Method 'initialize' with 'nom' and 'sou' as a parameter
+    function initialize ($name, $wage){
+        $this->name=$name;
+        $this->wage=$wage;
+    }    
+}
 ?>
