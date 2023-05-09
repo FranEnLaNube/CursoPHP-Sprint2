44ae26b51d46f9782383f0d3a73ac2ffe73a5f53
franenlanube <ciscosolana@gmail.com>
2023-05-09 21:30:12 +0200

    moved prompt from shape to this file

---
+++ Tema-4/Nivel1/T4N1E2/main.php
@@ -0,0 +1,6 @@
+<html>
+<h1>Tema 4, Nivel 1, Exercici 2:</h1>
+<h2>Enunciado:</h2>
+<p>Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la forma area().</p>
+<h2>Solució:</h2>
+</html>
