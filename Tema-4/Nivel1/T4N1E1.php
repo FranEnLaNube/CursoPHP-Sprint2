1d4fd6b40b1b669a6efb13248d9902d4093facb0
franenlanube <ciscosolana@gmail.com>
2023-05-09 16:02:02 +0200

    html section with exercise prompt

--- Tema-4/Nivel1/T4N1E1.php
+++ Tema-4/Nivel1/T4N1E1.php
@@ -1,3 +1,9 @@
+<html>
+<h1>Tema 4, Nivel 1, Exercici 1:</h1>
+<h2>Enunciado:</h2>
+<p>Crea una classe Employee, defineix com a atributs el seu nom i sou. Definir un mètode initialize que rebi com a paràmetres el nom i sou. Plantejar un segon mètode print que imprimeixi el nom i un missatge si ha de pagar o no impostos (si el sou supera 6000, paga impostos).</p>
+<h2>Solució</h2>
+
 <?php
 
 //Creating class employee
@@ -15,11 +21,14 @@ class Employee
     //Function print depending on the employee's wage
     function print($wage)
     {
-        return ($wage <= 6000 ?' no paga impuestos.' : ' paga impuestos.');
+        return ($wage <= 6000 ? ' no paga impuestos.' : ' paga impuestos.');
     }
 }
 //Object instance 
 $emp1 = new Employee;
-$emp1->initialize('Carlos', '7000');
+$emp1->initialize('Carlos', '6000');
 //Printing
-echo $emp1->name . $emp1->print($emp1->wage);
+echo $emp1->name . $emp1->print($emp1->wage)
+?>
+
+</html>
