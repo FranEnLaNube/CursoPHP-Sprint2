3e08ca166d2ef5c0eac27917c4fd217cdcef68b5
franenlanube <ciscosolana@gmail.com>
2023-05-10 13:40:05 +0200

    New abstract function area()

--- Tema-4/Nivel1/T4N1E2/shape.php
+++ Tema-4/Nivel1/T4N1E2/shape.php
@@ -1,12 +1,16 @@
 <?php
 abstract class Shape
-{//class properties
+{ //class properties
     protected $height;
     protected $width;
-//Constructor
+    //Constructor
     function __construct($height, $width)
     {
         $this->height = $height;
         $this->$width = $width;
     }
+    //abstract function to get area in inherited classes
+    abstract protected function area($height, $width);
+} {
+    # code...
 }
