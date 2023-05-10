491ee8abcd678926705be02b678ffe2636cc52fe
franenlanube <ciscosolana@gmail.com>
2023-05-10 15:23:45 +0200

    class Rectangle with its own function area()

--- Tema-4/Nivel1/T4N1E2/rectangle.php
+++ Tema-4/Nivel1/T4N1E2/rectangle.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 require 'shape.php';
 class Rectangle extends Shape
-{
-function area($height, $width){
-    return $this->$height * $this->$width;
+    {
+    function area()
+    {
+        return $this->height * $this->width;
+    }
 }
-}
-?>
