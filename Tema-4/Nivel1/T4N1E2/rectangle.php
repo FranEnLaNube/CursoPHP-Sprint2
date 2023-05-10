94907f593b2e21204668538a4b5cd3f41b6418bf
franenlanube <ciscosolana@gmail.com>
2023-05-10 16:01:39 +0200

    add coments and fix indentation

--- Tema-4/Nivel1/T4N1E2/rectangle.php
+++ Tema-4/Nivel1/T4N1E2/rectangle.php
@@ -1,8 +1,10 @@
 <?php
 //add require_once to avoid conflicts calling it in main.php
 require_once 'shape.php';
+//Class Rectangle inherits properties from Shape
 class Rectangle extends Shape
 {
+    //Rectangle's own function
     function area()
     {
         return $this->height * $this->width;
