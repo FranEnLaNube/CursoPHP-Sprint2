11b950ba828bd9c1c7ed2f3f1a63eb0cf5aef3f9
franenlanube <ciscosolana@gmail.com>
2023-05-12 01:29:28 +0200

    add some coments

--- Tema-5/Nivel1/T5N1E1.php
+++ Tema-5/Nivel1/T5N1E1.php
@@ -3,16 +3,21 @@
 <p>
     Necessitem crear un tipus de dades que representi un animal. Els animals tenen un nom, ara bé, no és el mateix el so de la “parla” d’un gos, que el d’un gat. Per tant, necessitem crear altres tipus de dades que ens ajudin a programar aquests comportaments. Bàsicament, volem un mètode makeSound() que mostri un missatge diferent si es tracta d’un gos (per exemple,“Bup, bup!”) o un gat (per exemple “Meu!”).
 </p>
+//Interface Animal which is implemented for Gos and Gat
 <?php interface Animal
 {
+    //makeSound methode empty and public
     public function makeSound();
 }
+//Class Gos who implements Animal to use makeSound in it.
 class Gos implements Animal {
+    //Own property
     public $name;
     public function makeSound(){
         echo "Bup, bup!";
     }
 }
+//Class gat similar to Gos
 class Gat implements Animal {
     public $name;
     public function makeSound(){
