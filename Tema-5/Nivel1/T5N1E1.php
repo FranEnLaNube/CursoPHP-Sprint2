0d746ec71ed928a65a7548f840ba421c3cd4ed9f
franenlanube <ciscosolana@gmail.com>
2023-05-12 01:37:38 +0200

    add <body> tag and fix a comment outside php tag in line 7

--- Tema-5/Nivel1/T5N1E1.php
+++ Tema-5/Nivel1/T5N1E1.php
@@ -1,10 +1,12 @@
 <html>
+<body>
 <h1>Exercici 1</h1>
 <p>
     Necessitem crear un tipus de dades que representi un animal. Els animals tenen un nom, ara bé, no és el mateix el so de la “parla” d’un gos, que el d’un gat. Per tant, necessitem crear altres tipus de dades que ens ajudin a programar aquests comportaments. Bàsicament, volem un mètode makeSound() que mostri un missatge diferent si es tracta d’un gos (per exemple,“Bup, bup!”) o un gat (per exemple “Meu!”).
 </p>
+<?php 
 //Interface Animal which is implemented for Gos and Gat
-<?php interface Animal
+interface Animal
 {
     //makeSound methode empty and public
     public function makeSound();
@@ -23,5 +25,7 @@ class Gat implements Animal {
     public function makeSound(){
         echo "Mèu";
     }
+}
 ?>
+</body>
 </html>
