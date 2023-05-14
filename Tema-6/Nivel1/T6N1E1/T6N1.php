d59457d4839d77cc706dff0c871d4d70ec0809a5
franenlanube <ciscosolana@gmail.com>
2023-05-14 18:44:09 +0200

    margin-top: 2em add to h1 and p

--- Tema-6/Nivel1/T6N1E1/T6N1.php
+++ Tema-6/Nivel1/T6N1E1/T6N1.php
@@ -1,2 +1,14 @@
-<?php 
-echo "El nombre del usuario es ".$_POST["name"];?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salida Formulario - Ejercicio 1, Nivel 1, Tema 6, Sprint 1, PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Formulario completado</h1>
+    <p>Hola <?php echo $_POST['name']?>, elegiste <?php echo $_POST['language']?>!, Suerte con eso...</p>
+</body>
