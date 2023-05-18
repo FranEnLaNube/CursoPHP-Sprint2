88dccdefbcab4a43b17c463efad1313a1158e79c
franenlanube <ciscosolana@gmail.com>
2023-05-18 12:25:16 +0200

    First commit for NumberCheckerTest and upload a .gitignore

---
+++ Tema-7/Nivel1/T7N1E1/tests/unit/NumberCheckerTest.php
@@ -0,0 +1,39 @@
+use PHPUnit\Framework\Testcase;
+
+class NumberCheckerTest extends NumberChecker
+{
+
+}
+<!-- 
+
+Clase original a chequear
+    
+class NumberChecker  {
+
+
+public function __construct(private int $number){}
+
+public function isEven(): bool {
+    return $this->number%2 == 0;
+}
+public function isPositive(): bool {
+    return $this->number > 0;
+}
+
+Exemple de test tipus
+
+2. Tamany d’entrada de dades
+    ● Cas 0 elements. Què passa si no entra res.
+    ● Cas 1 element.
+    ● Cas menor més interessant.
+    ● Cas major més interessant.
+3. Dicotomies
+    ● Parell/Imparell
+    ● Positiu/Negatiu/0
+    ● Vocal/Consonant
+    ● Buit/No buit
+4. Ordre
+    ● ¿L’ordre d’entrada de dades,varia el resultat a retornar?
+5. Limits
+    ● Tenir en compte els valors llindar(si existeixen)
+} -->
