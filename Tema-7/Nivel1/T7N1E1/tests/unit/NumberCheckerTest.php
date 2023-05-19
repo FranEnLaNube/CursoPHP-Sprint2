36ed7f08d388177f7dd933d2757cee918f4b2152
franenlanube <ciscosolana@gmail.com>
2023-05-20 01:47:27 +0200

    new test testIsPositive()

--- Tema-7/Nivel1/T7N1E1/tests/unit/NumberCheckerTest.php
+++ Tema-7/Nivel1/T7N1E1/tests/unit/NumberCheckerTest.php
@@ -8,12 +8,20 @@ class NumberCheckerTest extends TestCase
 {
     //Método de prueba para verificar si el número es par
     public function testIsEven() : void
+    {
         //Creando una instancia de la clase NumberChecker
         $numberChecker = new NumberChecker(10);
 
         //Comprobando si el número es par
         $this->assertTrue($numberChecker->isEven(), 'el número debería ser par');
-
+    }
+    //Método para probar si es positivo
+    public function testIsPositive() : void
+    {
+        //Nuevo instancia de NumberChecker
+        $numberChecker = new NumberChecker(-5);
+        //Comprobando si es negativo
+        $this->assertFalse($numberChecker->isPositive(), 'El número debería ser negativo');
     }
 ?>
 
