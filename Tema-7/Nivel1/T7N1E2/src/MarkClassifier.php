cde70203a62c0f8b1aca900163f5517ce2d6f10a
franenlanube <ciscosolana@gmail.com>
2023-05-20 18:40:47 +0200

    add if (is_numeric($mark) validation

--- Tema-7/Nivel1/T7N1E2/src/MarkClassifier.php
+++ Tema-7/Nivel1/T7N1E2/src/MarkClassifier.php
@@ -3,18 +3,22 @@ Class MarkClassifier
 {
     function classifier($mark)
     {
-        if ($mark >= 0 && $mark <= 100) {
-            if ($mark >= 60) {
-                return 'Primera Divisió';
-            } elseif ($mark >= 45) {
-                return 'Segona Divisió';
-            } elseif ($mark >= 33) {
-                return 'Tercera Divisió';
+        if (is_numeric($mark)) {            
+            if ($mark >= 0 && $mark <= 100) {
+                if ($mark >= 60) {
+                    return 'Primera Divisió';
+                } elseif ($mark >= 45) {
+                    return 'Segona Divisió';
+                } elseif ($mark >= 33) {
+                    return 'Tercera Divisió';
+                } else {
+                    return 'Suspenet';
+                }
             } else {
-                return 'Suspenet';
+                return 'Nota fora de rang';
             }
         } else {
-        return 'Nota fora de rang';
+            return 'Entrada invàlida';
         }
     }
 }
