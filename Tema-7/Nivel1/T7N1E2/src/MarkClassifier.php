4e2d764f12c7663d1c3e882be4d11d042bf9c9c0
franenlanube <ciscosolana@gmail.com>
2023-05-20 17:45:41 +0200

    First Classifier function prototipe

---
+++ Tema-7/Nivel1/T7N1E2/src/MarkClassifier.php
@@ -0,0 +1,22 @@
+<?php
+Class MarkClassifier
+{
+    function classifier($mark)
+    {
+        if ($mark >= 0 && $mark <= 100) {
+            if ($mark >= 60) {
+                return 'Primera Divisió';
+            } elseif ($mark >= 45) {
+                return 'Segona Divisió';
+            } elseif ($mark >= 33) {
+                return 'Tercera Divisió';
+            } else {
+                return 'Suspenet';
+            }
+        } else {
+        return 'Nota fora de rang';
+        }
+    }
+}
+echo classifier(-120)
+?>
