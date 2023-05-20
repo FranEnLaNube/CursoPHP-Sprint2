90c6c6ac90415e17b32bd48c46829b5c1b76b832
franenlanube <ciscosolana@gmail.com>
2023-05-20 03:27:38 +0200

    improve readability

--- Tema-7/Nivel1/T7N1E1/tests/unit/NumberCheckerTest.php
+++ Tema-7/Nivel1/T7N1E1/tests/unit/NumberCheckerTest.php
@@ -1,6 +1,6 @@
 <?php 
 //Archivo que se va a probar
-require '../../src/NumberChecker.php';
+require 'src/NumberChecker.php';
 //Importando la clase testeadora de PHPUnit
 use PHPUnit\Framework\TestCase;
 //Definir la clase de pruebas
@@ -9,20 +9,31 @@ class NumberCheckerTest extends TestCase
     //Método de prueba para verificar si el número es par
     public function testIsEven() : void
     {
-        //Creando una instancia de la clase NumberChecker
-        $numberChecker = new NumberChecker(10);
+        // Arrange: Preparación de los datos de prueba
 
-        //Comprobando si el número es par
-        $this->assertTrue($numberChecker->isEven(), 'el número debería ser par');
+        //almacenando el resultado de prueba en variable
+        $number = 10;
+        $numberChecker = new NumberChecker($number);
+        
+        // Act: Ejecutando el método a probar
+        $result = $numberChecker->isEven();
+        
+        // Assert: Usando el test
+        $this->assertTrue($result, 'el número debería ser par');
     }
     //Método para probar si es positivo
     public function testIsPositive() : void
     {
-        //Nuevo instancia de NumberChecker
-        $numberChecker = new NumberChecker(-5);
+        // Arrange: Preparación de los datos de prueba
+        $number = -5;
+        $numberChecker = new NumberChecker($number);
+        
+        // Act: Ejecutando el método a probar
+        $result = $numberChecker->isPositive();
+        
+        // Assert: Usando el test
+        $this->assertFalse($result, 'el número debería ser negativo');
         //Comprobando si es negativo
-        $this->assertFalse($numberChecker->isPositive(), 'El número debería ser negativo');
     }
 }    
 ?>
-
