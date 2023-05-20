7b0bee16f7d3436a621b123c5e56c8e24e7aa767
franenlanube <ciscosolana@gmail.com>
2023-05-20 02:13:03 +0200

    fix require path

--- Tema-7/Nivel1/T7N1E1/tests/unit/NumberCheckerTest.php
+++ Tema-7/Nivel1/T7N1E1/tests/unit/NumberCheckerTest.php
@@ -1,6 +1,6 @@
 <?php 
 //Archivo que se va a probar
-require 'src/NumberChecker.php';
+require '../../src/NumberChecker.php';
 //Importando la clase testeadora de PHPUnit
 use PHPUnit\Framework\TestCase;
 //Definir la clase de pruebas
@@ -23,38 +23,6 @@ class NumberCheckerTest extends TestCase
         //Comprobando si es negativo
         $this->assertFalse($numberChecker->isPositive(), 'El número debería ser negativo');
     }
+}    
 ?>
 
-<!-- 
-
-Clase original a chequear
-    
-class NumberChecker  {
-
-    
-    public function __construct(private int $number){}
-    
-    public function isEven(): bool {
-        return $this->number%2 == 0;
-}
-public function isPositive(): bool {
-    return $this->number > 0;
-}
-
-Exemple de test tipus
-
-2. Tamany d’entrada de dades
-    ● Cas 0 elements. Què passa si no entra res.
-    ● Cas 1 element.
-    ● Cas menor més interessant.
-    ● Cas major més interessant.
-    3. Dicotomies
-    ● Parell/Imparell
-    ● Positiu/Negatiu/0
-    ● Vocal/Consonant
-    ● Buit/No buit
-    4. Ordre
-    ● ¿L’ordre d’entrada de dades,varia el resultat a retornar?
-5. Limits
-    ● Tenir en compte els valors llindar(si existeixen)
-} -->
