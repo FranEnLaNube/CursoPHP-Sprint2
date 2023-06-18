a69be1649e583cd3cb2af4bb759a6f31f4b9b03e
franenlanube <ciscosolana@gmail.com>
2023-06-18 21:37:22 +0200

    Fix mistake in comment

--- Tema-3/Nivel1/T3N1E1.php
+++ Tema-3/Nivel1/T3N1E1.php
@@ -1,7 +1,7 @@
 <?php 
 //Creating $myArray
 $myArray= [2,4,6,5,9];
-//Printing $myArray with a for loop that finiches with the count() function
+//Printing $myArray with a for loop that finishes with the count() function
 for ($i=0; $i < count($myArray); $i++) { 
     echo $myArray[$i].'<br>';
   }
