42c30a879da89a1fbf85d473d16cc23a69b6714f
franenlanube <ciscosolana@gmail.com>
2023-07-10 01:33:41 +0200

    delete files

--- Tema-3/Nivel1/T3N1E1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//Creating $myArray
-$myArray= [2,4,6,5,9];
-//Printing $myArray with a for loop that finishes with the count() function
-for ($i=0; $i < count($myArray); $i++) { 
-    echo $myArray[$i].'<br>';
-  }
-?>
