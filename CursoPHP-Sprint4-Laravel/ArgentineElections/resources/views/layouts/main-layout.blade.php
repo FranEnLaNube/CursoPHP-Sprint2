2536d10b1fb3a57daf70b7e96378f77c1bc7d127
franenlanube <ciscosolana@gmail.com>
2023-09-28 12:26:15 +0200

    Add register, login, dashboard buttons and crud panel tabs

--- CursoPHP-Sprint4-Laravel/ArgentineElections/resources/views/layouts/main-layout.blade.php
+++ CursoPHP-Sprint4-Laravel/ArgentineElections/resources/views/layouts/main-layout.blade.php
@@ -18,14 +18,68 @@
     <header class="bg-gray-800 text-white font-bold py-2 px-4">
         <nav class="flex justify-between items-center">
             <a href="/"
-                class="home-link bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Home</a>
+                class="home-link bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">Home</a>
+            @if (Route::has('login'))
+                <div class="">
+                    @auth
+                        <a href="{{ url('/dashboard') }}"
+                            class=" bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}"
+                            class=" bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}"
+                                class=" bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
         </nav>
     </header>
+    <ul class="flex">
+        <li class="flex-1 mb-1">
+            <a class="py-2 px-4 text-center block border-2 rounded-b font-bold border-blue-500
+                {{ request()->is('results*')
+                    ? 'bg-blue-500 text-white hover:bg-blue-700 hover:border-blue-700'
+                    : 'text-blue-500 bg-white hover:bg-blue-200' }}"
+                href="/results">General results</a>
+        </li>
+        <li class="flex-1 mb-1 mx-1">
+            <a class="py-2 px-4 text-center block border-2 rounded-b font-bold border-blue-500
+                {{ request()->is('elections*')
+                    ? 'bg-blue-500 text-white hover:bg-blue-700 hover:border-blue-700'
+                    : 'text-blue-500 bg-white hover:bg-blue-200' }}"
+                href="/elections">Elections</a>
+        </li>
+        <li class="flex-1 mb-1 mx-1">
+            <a class="py-2 px-4 text-center block border-2 rounded-b font-bold border-blue-500
+                {{ request()->is('alternatives*')
+                    ? 'bg-blue-500 text-white hover:bg-blue-700 hover:border-blue-700'
+                    : 'text-blue-500 bg-white hover:bg-blue-200' }}"
+                href="/alternatives">Candidates</a>
+        </li>
+        <li class="flex-1 mb-1">
+            @auth
+                <a class="py-2 px-4 text-center block border-2 rounded-b font-bold border-blue-500
+                {{ request()->is('votes*')
+                    ? 'bg-blue-500 text-white hover:bg-blue-700 hover:border-blue-700'
+                    : 'text-blue-500 bg-white hover:bg-blue-200' }} "
+                    href="/votes/create">Votes manager</a>
+            @else
+                <a class="py-2 px-4 text-center block border-2 rounded-b font-bold border-blue-500 cursor-not-allowed
+                    {{ request()->is('votes*')
+                        ? 'bg-blue-500 text-white hover:bg-blue-700 hover:border-blue-700'
+                        : 'text-blue-500 bg-white hover:bg-blue-200' }}"
+                    onclick="return alert('You need to be logged in to access here')">Votes manager</a>
+            @endauth
+        </li>
+    </ul>
+
 
     <main class="flex-grow">
         @yield('content')
     </main>
-    {{-- TODO: Add a footer component--}}
     <footer class="bg-gray-800 text-white p-3 text-center mt-auto">
         &copy; <?= date('Y') ?> Argentine Elections Results
     </footer>
