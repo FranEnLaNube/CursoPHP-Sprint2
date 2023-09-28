8aba2e73c8999730bf47b7f4cbc009d2e0248739
franenlanube <ciscosolana@gmail.com>
2023-09-28 12:30:56 +0200

    Implement authenticated routes for votes, alternatives and elections

--- CursoPHP-Sprint4-Laravel/ArgentineElections/routes/web.php
+++ CursoPHP-Sprint4-Laravel/ArgentineElections/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\AlternativesController;
+use App\Http\Controllers\ElectionsController;
 use App\Http\Controllers\ResultsController;
 use App\Http\Controllers\VotesController;
-use App\Models\Election;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,27 +17,23 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// Route for Home
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/results');
+    // Original: return view('welcome');
 });
 
 Route::resource('alternatives', AlternativesController::class);
 
-Route::resource('elections',ElectionController::class);
-//TODO ¿Delete provinces routes?
-Route::resource('provinces','App\Http\Controllers\ProvincesController');
+Route::resource('elections', ElectionsController::class);
 
-Route::resource('votes', VotesController::class);
+//Route::resource('votes', VotesController::class);
 
 // Show a particular register of a vote
 Route::get('votes/{election_id}/{province_id}/{alternative_id}', [VotesController::class, 'showVote'])->name('votes.showVote');
 
-// Delete/destroy a particular register of a vote
-Route::delete('votes/{election_id}/{province_id}/{alternative_id}', [VotesController::class, 'destroy'])->name('votes.destroy-votes');
-
-// Edit a particular register of a vote
-Route::get('votes/{year}/{province_id}/{alternative_id}/edit', [VotesController::class, 'edit'])->name('votes.edit');
+// ShowElections method, shows elections with associated votes
+Route::get('results/', [ResultsController::class, 'showElections'])->name('results.showElections');
 
 // ShowByYear
 Route::get('results/{year}', [ResultsController::class, 'showByYear'])->name('results.showByYear');
@@ -53,4 +49,14 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    // Alternatives
+    Route::resource('alternatives', AlternativesController::class)->only(['create', 'edit', 'destroy']);
+    // Elections
+    Route::resource('elections', ElectionsController::class)->only(['create', 'edit', 'destroy']);
+    // Votes
+    Route::resource('votes', VotesController::class);
+    // Edit a particular register of a vote
+    Route::get('votes/{year}/{province_id}/{alternative_id}/edit', [VotesController::class, 'edit'])->name('votes.edit');
+    // Delete/destroy a particular register of a vote
+    Route::delete('votes/{election_id}/{province_id}/{alternative_id}', [VotesController::class, 'destroy'])->name('votes.destroy-votes');
 });
